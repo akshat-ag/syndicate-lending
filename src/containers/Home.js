9a13f5977f30e020701d1e14288e38300a821848
Ashley224 <ashishagrawal224@gmail.com>
2018-08-16 12:27:02 +0530

    Input Changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,7 @@ import {Route, Redirect, Switch} from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Footer from '../presentation/Footer';
 import LoanApplication from './LoanApplication';
+import LoanDetails from './LoanDetails';
 
 
 class Home extends Component {
@@ -19,9 +20,12 @@ class Home extends Component {
                     <Redirect exact from="/" to="/dashboard"/>
                     <Route path="/dashboard" component={Dashboard} />
                     <Route path="/application" component={LoanApplication} />
+                    <Route exact path="/loan/:id" component={LoanDetails}/>
+
+
                 </Switch>
             </div>
-            <Footer/>
+           
             </React.Fragment>
 
         );
