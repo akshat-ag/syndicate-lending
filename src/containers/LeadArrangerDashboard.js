985b64197a9ff7a6c739dc138e008f4ea913a75d
Ashley224 <ashishagrawal224@gmail.com>
2018-08-29 16:29:43 +0530

    Lead Arranger Dashoboard

---
+++ src/containers/LeadArrangerDashboard.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import {AuthenticatedServiceInstance} from '../services/AuthenticationService';
+import PendingApplications from '../presentation/BorrowerPendingApplications';
+import BiddingLoans from '../presentation/BiddingLoans';
+import BorrowerAcceptedLoans from '../presentation/BorrowerAcceptedLoans';
+import { Redirect } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+class LeadArrangerDashboard extends Component {
+    constructor(props) {
+    	super(props);
+    	this.state = {
+            quotedLoans: [],
+            acceptedLoans: [],
+            biddingLoans: []
+    	};
+        this.myCallback = this.myCallback.bind(this);
+        this.handleAccept = this.handleAccept.bind(this);
+        this.handleRate = this.handleRate.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        
+    }
+    componentDidMount() {
+        const arrangerName = 'citi';
+        let loans =  axios.get('http://delvmplwindpark00:8080/requisitions/la/' + arrangerName)
+        .then(({ data: loanList }) => {
+         // console.log('user', loanList);
+        let approvedLoans = [];
+        let biddingLoans = [];
+        for( let i = 0, max = loanList.length; i < max ; i++ ){
+            if( loanList[i].RequisitionStatus === "Approved" ){
+                approvedLoans.push(loanList[i]);
+            } else {
+                biddingLoans.push(loanList[i]);
+            }
+        } 
+        this.setState({ acceptedLoans: approvedLoans, biddingLoans: biddingLoans });
+                     });
+    }
+    myCallback(loanId){
+    	this.setState({
+    		redirect: true,
+    		loanToRedirect: loanId
+    	});
+    }
+    
+    
+    handleAccept(loanId){
+        let postObj = this.state.quotedLoans.find(x => x.RequisitionNo === loanId);
+        // if(postObjIndex === -1) {
+        //     NotificationManager.error('Error', 'Requisition Confirmed');
+        // }
+        
+        postObj.status = 'Rate Quoted';
+        
+          axios.put(`http://delvmplwindpark00:8080/updateRequisition/`,  postObj )
+            .then(res => {
+                this.setState({redirect: true})
+              console.log(res);
+              console.log(res.data);
+              this.refreshPage();
+            });
+    };
+    handleRate = function(e,prop) { 
+        let loanObj = this.state.quotedLoans.findIndex(x => x.RequisitionNo === prop);
+        if(loanObj === -1) {
+            let obj = {
+             RequisitionNo:prop,
+             leadArranger:'citi',
+             rate: e.target.value }
+             this.setState({
+                quotedLoans: [...this.state.quotedLoans, obj]
+            });
+        } else {
+            let value = e.target.value;
+            this.setState((prevState) => {
+                const newItems = [...prevState.quotedLoans];
+                newItems[loanObj].rate = value;
+                return {quotedLoans: newItems};
+            });
+            
+        }
+    }
+    handleCancel(loanId) {
+        let postObjIndex = this.state.quotedLoans.findIndex(x => x.RequistionNo === loanId);
+        let postObj= {};
+        postObj.action =  "decline";
+        if(postObjIndex === -1) {
+            postObj.RequistionNo = loanId;
+        } else {
+           postObj = this.state.quotedLoans[postObjIndex];
+           delete postObj.rateQuoted;
+        }
+        axios.post(`citi/${loanId}`, { postObj })
+            .then(res => {
+              console.log(res);
+              console.log(res.data);
+              this.refreshPage();
+            });
+    }
+    refreshPage() {
+        console.log("Clicked");
+        window.location.reload();
+    }
+    render() {
+        const biddingHeader = "Loan(s) for Bidding";
+        const accepteddHeader = "Borrower Accepted Loans";
+        console.log(this.state);
+   		if (this.state.redirect && this.state.loanToRedirect) {
+    		return <Redirect push to={`/dashboard`}/>;
+  		}
+        if(this.state.acceptedLoans.length > 0) {
+            return (
+                <div id="leadArrangerDashboard">
+                             
+                  <BiddingLoans heading={biddingHeader} loanList={this.state.biddingLoans} changeRate={this.handleRate} onAccept={this.handleAccept} onDecline={this.handleCancel}/>
+                  <BorrowerAcceptedLoans heading={accepteddHeader} loanList={this.state.acceptedLoans} showLoan={this.showLoanDetails} generateInfo={this.generateInfo}/>
+                </div>
+            );
+        }
+        else {
+            return <div><CircularProgress  size={50} thickness={7} />Loading....</div>
+        }
+    }
+}
+
+export default LeadArrangerDashboard;
---
+++ src/presentation/BiddingLoans.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+const styles = theme => ({
+  root: {
+   width: '90%',
+   marginTop: theme.spacing.unit * 3,
+   overflowX: 'auto',
+ }, table: {
+   minWidth: 100,
+ },
+ paddingNone: {
+   padding: '1px 56px 1px 24px',
+ }
+});
+function BiddingLoans(props) {
+  const {classes} = props;
+  let rate;
+  const sendBid = function (loanId) {
+    const postObj = [loanId , rate];
+    console.log("postobj",postObj);
+    props.onAccept(postObj);
+};
+  return (
+    <div className="root1">
+      <h3> {props.heading}</h3>
+      <Paper className={classes.root}>
+     
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Loan Id</TableCell>
+            <TableCell >Borrower First Name</TableCell>
+            <TableCell >Borrower Second Name</TableCell>
+            <TableCell >Loan Amount</TableCell>
+            <TableCell >Deadline</TableCell>
+            <TableCell >Interest Rate</TableCell>
+            <TableCell ></TableCell>
+            <TableCell ></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(props.loanList).map(row => {
+            return (
+              <TableRow key={props.loanList[row].RequisitionNo}>
+                <TableCell component="th" scope="row">
+                  {props.loanList[row].RequisitionNo}
+                </TableCell>
+                <TableCell numeric>{props.loanList[row].FirstName}</TableCell>
+                <TableCell numeric>{props.loanList[row].LastName}</TableCell>
+                <TableCell numeric>{props.loanList[row].RequisitionAmount}</TableCell>
+                <TableCell numeric>{props.loanList[row].EndDate}</TableCell>
+
+                <TableCell numeric>
+                <TextField
+                required
+                id="simple-start-adornment"
+                value={rate}
+                onChange={(e) => props.changeRate(e,props.loanList[row].RequisitionNo)}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">%</InputAdornment>,
+                }}
+              />
+                </TableCell>
+                <TableCell numeric><Button id="acceptBtn" type="submit" onClick={() => {props.onAccept(props.loanList[row].RequisitionNo);}}>Accept</Button></TableCell>
+                <TableCell numeric><Button variant="contained" color="secondary" onClick={() => {props.onDecline(props.loanList[row].RequisitionNo);}}>Decline</Button></TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+     
+    </Paper>
+    </div>)
+}
+
+export default withStyles(styles)(BiddingLoans);
