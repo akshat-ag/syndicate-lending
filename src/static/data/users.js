972a4301008ee352e6b5f0b72010729a1fb3b4f0
Ashley224 <ashishagrawal224@gmail.com>
2018-09-10 17:04:48 +0530

    Common Files

---
+++ src/static/data/users.js
@@ -0,0 +1,30 @@
+export const users = [{
+        "id" : "1",
+        "name" : "Mukesh Ambani",
+        "organisationId" : "reliance",
+        "username" : "reliance",
+        "password" : "reliance",
+        "role" : "borrower"
+    },{
+        "id" : "2",
+        "name" : "Patrick Sprows",
+        "organisationId" : "citi",
+        "username" : "citi",
+        "password" : "bank",
+        "role" : "bank"
+    },{
+        "id" : "3",
+        "name" : "Adam McLory",
+        "organisationId" : "wells",
+        "username" : "wells",
+        "password" : "fargo",
+        "role" : "bank"
+    },{
+        "id" : "4",
+        "name" : "Billy Brown",
+        "organisationId" : "jp",
+        "username" : "jp",
+        "password" : "morgan",
+        "role" : "bank"
+    }
+  ];
